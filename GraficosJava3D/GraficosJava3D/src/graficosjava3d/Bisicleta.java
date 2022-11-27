c692af83a2aa13a3f7417c6f72261a8075f66c2a
yeison123 <yeison_123.2@hotmail.com>
2022-11-27 15:34:29 -0500

    nuevo_3d

---
+++ GraficosJava3D/GraficosJava3D/src/graficosjava3d/Bisicleta.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graficosjava3d;
+
+/**
+ *
+ * @author Soporte
+ */
+import com.sun.j3d.loaders.IncorrectFormatException;
+import com.sun.j3d.loaders.ParsingErrorException;
+import com.sun.j3d.loaders.Scene;
+import com.sun.j3d.loaders.objectfile.ObjectFile;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import javax.media.j3d.Alpha;
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.DirectionalLight;
+import javax.media.j3d.Light;
+import javax.media.j3d.RotationInterpolator;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.AxisAngle4f;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+import javax.vecmath.Vector3f;
+
+public class Bisicleta extends Applet{
+    private SimpleUniverse universo = null;
+    private Canvas3D lienzo = null;
+    
+    public Bisicleta(){
+        setLayout(new BorderLayout());
+        GraphicsConfiguration gc = SimpleUniverse.getPreferredConfiguration();
+        lienzo = new Canvas3D(gc);
+        add("Center",lienzo);
+        universo = new SimpleUniverse(lienzo);
+        BranchGroup escena = crearEscenarioGrafico(); 
+        universo.addBranchGraph(escena);
+    }
+    
+    private BranchGroup crearEscenarioGrafico(){
+        BranchGroup objetoRaiz = new BranchGroup();
+        BoundingSphere fronteras = new BoundingSphere(new Point3d(0,0,0),200);
+        TextureLoader archivo = new TextureLoader("src/images/Fondo_Bisicleta.jpg",this);
+        Background fondo = new Background(archivo.getImage());
+        fondo.setApplicationBounds(fronteras);
+        objetoRaiz.addChild(fondo);
+        objetoRaiz.addChild(modelarObjeto());
+        objetoRaiz.addChild(iluminar());
+        return objetoRaiz;
+    }
+    
+    private BranchGroup modelarObjeto(){
+        BranchGroup objetoRaiz = new BranchGroup();
+        TransformGroup grupo = new TransformGroup();
+        Transform3D transformacion3d = new Transform3D();
+        transformacion3d.setTranslation(new Vector3d(0.3,-0.15,-2)); //define posicion del objeto
+        transformacion3d.setScale(0.01); //define el tamaño del objeto
+        grupo.setTransform(transformacion3d);
+        TransformGroup producto = new TransformGroup();
+        producto.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        producto.addChild(cargarObjeto("src\\images\\11717_bicycle_v2_L1.obj"));
+        
+        Alpha alpha = new Alpha(-1,20000);
+        
+        Transform3D eje = new Transform3D();
+        eje.setRotation(new AxisAngle4f(8.3f,8.0f,8.0f,20.0f));
+        RotationInterpolator girar = new RotationInterpolator(alpha,producto,eje,0.0f,(float) Math.PI*(6.0f));
+        BoundingSphere fronteras = new BoundingSphere(new Point3d(0.0,0.0,0.0),1000.0);
+        girar.setSchedulingBounds(fronteras);
+        producto.addChild(girar);
+        grupo.addChild(producto);
+        objetoRaiz.addChild(grupo);
+        objetoRaiz.addChild(iluminar());
+        objetoRaiz.compile();
+        
+        return objetoRaiz;
+    }
+    
+    private BranchGroup cargarObjeto(String filename){
+        BranchGroup objetoRaiz = new BranchGroup();
+        TransformGroup grupo = new TransformGroup();
+        ObjectFile cargar = new ObjectFile();
+        
+        Scene s = null;
+        File archivo = new java.io.File(filename);
+        
+        try {
+           s = cargar.load(archivo.toURI().toURL());
+        } catch(IncorrectFormatException | ParsingErrorException | FileNotFoundException | MalformedURLException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+        grupo.addChild(s.getSceneGroup());
+        objetoRaiz.addChild(grupo);
+        objetoRaiz.compile();
+        
+        return objetoRaiz;
+    }
+    
+    private Light iluminar(){
+        DirectionalLight luz = new DirectionalLight(true,new Color3f(2.0f,1.0f,0.0f),new Vector3f(-2.0f,3.0f,-2.0f));
+        luz.setInfluencingBounds(new BoundingSphere(new Point3d(),1000.0));
+        
+        return luz;
+    }
+    
+    public static void main (String[] args){
+        Bisicleta ejecutar = new Bisicleta();
+        Frame ventana = new MainFrame(ejecutar,748,385);
+    }
+}
---
+++ GraficosJava3D/GraficosJava3D/src/graficosjava3d/Silla.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficosjava3d;
+
+/**
+ *
+ * @author julian.ruiz.aya
+ */
+
+import com.sun.j3d.loaders.IncorrectFormatException;
+import com.sun.j3d.loaders.ParsingErrorException;
+import com.sun.j3d.loaders.Scene;
+import com.sun.j3d.loaders.objectfile.ObjectFile;
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import java.io.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+
+public final class Silla extends Applet {
+    private static final long serialVersionUID = 1L;
+    
+    public static void main(String[] args){
+        if (args.length>=1)
+            new MainFrame(new Silla(args[1]),850,600);
+        else
+            new MainFrame(new Silla(null),850,600);
+    }
+    
+    public Silla (String filename){
+        //se crea el universo
+        setLayout(new BorderLayout());
+        GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+        Canvas3D c = new Canvas3D(config);
+        add("Center",c);
+        
+        //se crea el metodo de la escena y se une al universo
+        BranchGroup escena = crearescenaG(filename);
+        SimpleUniverse u = new SimpleUniverse(c);
+        u.getViewingPlatform().setNominalViewingTransform();
+        u.addBranchGraph(escena);
+    }
+    BoundingBox limites = new BoundingBox(new Point3d(-80,-100,-100),new Point3d(200,100,100));
+    
+    public BranchGroup crearescenaG(String filename){
+        BranchGroup escenaRoot = new BranchGroup();
+        escenaRoot.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+        escenaRoot.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        escenaRoot.addChild(dibujarfondo());
+        
+        //ambiente de luces
+        Color3f luz1Color = new Color3f(1.0f,1.0f,0.9f);
+        Vector3f luz1Direccion = new Vector3f(4.0f,-7.0f,-12.0f);
+        Color3f luz2Color = new Color3f(0.3f,0.3f,0.4f);
+        Vector3f luz2Direccion = new Vector3f(-6.0f,-2.0f,-1.0f);
+        Color3f ambienteColor = new Color3f(0.1f,0.1f,0.1f);
+        
+        AmbientLight Nodoambienteluces = new AmbientLight(ambienteColor);
+        Nodoambienteluces.setInfluencingBounds(limites);
+        escenaRoot.addChild(Nodoambienteluces);
+        
+        //direccion de las luces
+        DirectionalLight luz1 = new DirectionalLight(luz1Color,luz1Direccion);
+        luz1.setInfluencingBounds(limites);
+        escenaRoot.addChild(luz1);
+        
+        DirectionalLight luz2 = new DirectionalLight(luz2Color,luz2Direccion);
+        luz2.setInfluencingBounds(limites);
+        escenaRoot.addChild(luz2);
+        
+        TransformGroup escenaTG = new TransformGroup();
+        escenaTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        escenaRoot.addChild(escenaTG);
+        
+        Alpha rotacionAlpha = new Alpha(-1,Alpha.INCREASING_ENABLE,0,0,4000,0,2,0,2,0);
+        
+        Transform3D yAxis = new Transform3D();
+        RotationInterpolator rotar = new RotationInterpolator(rotacionAlpha,escenaTG,yAxis,1.0f,(float) Math.PI*2.0f);
+        rotar.setSchedulingBounds(limites);
+        escenaTG.addChild(rotar);
+        
+        //cargar objeto
+        TransformGroup objTG = new TransformGroup();
+        Transform3D objTrans = new Transform3D();
+        objTG.getTransform(objTrans);
+        objTrans.setTranslation(new Vector3d(0.0,0.0,0.0));
+        objTrans.setScale(0.3);
+        objTG.setTransform(objTrans);
+        
+        escenaTG.addChild(objTG);
+        
+        int flags = ObjectFile.RESIZE;
+        ObjectFile file = new ObjectFile(flags,(float)(30.0*Math.PI/180.0));
+        
+         Scene s = null;
+        
+        try{
+           s = file.load(filename == null ? "src\\images\\Cadeira.obj" : filename);
+           //objTG.addChild(s.getSceneGroup());
+        }
+        catch (FileNotFoundException | ParsingErrorException | IncorrectFormatException e){
+            System.err.println(e);
+            System.exit(1);
+        }
+        
+        objTG.addChild(s.getSceneGroup());
+        return escenaRoot;
+    }
+    
+    private TransformGroup dibujarfondo(){
+        TransformGroup fondo = new TransformGroup();
+        TextureLoader espacio = new TextureLoader("src/images/Fondo_Silla.jpg",this);
+        Background back = new Background();
+        back.setImage(espacio.getImage());
+        back.setImageScaleMode(Background.SCALE_FIT_ALL);
+        back.setApplicationBounds(limites);
+        fondo.addChild(back);
+        
+        return fondo;
+    }
+    
+}
